220b569f6c240360fd8f39a2ae24fde853a36cba
Kwakhanya Nkonzombi <knkonzom@student.wethinkcode.co.za>
2019-09-03 03:10:58 +0200

    Fixed GNL.

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: knkonzom <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/01 20:09:51 by knkonzom          #+#    #+#             */
-/*   Updated: 2019/09/02 20:02:33 by knkonzom         ###   ########.fr       */
+/*   Updated: 2019/09/03 01:39:54 by knkonzom         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ static int	new_line(char **s, char **line, int fd, int ret)
 	if (s[fd][len] == '\n')
 	{
 		*line = ft_strsub(s[fd], 0, len);
-		temp = ft_strdup(s[fd] + len + 1);
+		temp = ft_strdup(s[fd] + (len + 1));
 		free(s[fd]);
 		s[fd] = temp;
 		if (s[fd][0] == '\0')
@@ -42,26 +42,28 @@ static int	new_line(char **s, char **line, int fd, int ret)
 int			get_next_line(const int fd, char **line)
 {
 	static char	*s[255];
-	char		buff[BUFF_SIZE + 1];
+	char		buf[BUFF_SIZE + 1];
 	char		*temp;
 	int			ret;
 
 	if (fd < 0 || line == NULL)
 		return (-1);
-	while ((ret = read(fd, buff, BUFF_SIZE) > 0))
+	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
 	{
-		buff[ret] = '\0';
+		buf[ret] = '\0';
 		if (s[fd] == NULL)
-			s[fd] = ft_strnew(1);
-		temp = ft_strjoin(s[fd], buff);
-		free (s[fd]);
-		s[fd] = temp;
-		if (ft_strchr(buff, '\n'))
-			break;
+			s[fd] = ft_strdup(buf);
+		else
+		{
+			temp = ft_strjoin(s[fd], buf);
+			free(s[fd]);
+			s[fd] = temp;
+		}
+		if (ft_strchr(buf, '\n'))
+			break ;
 	}
 	if (ret < 0)
 		return (-1);
-	else if (ret == 0 && (s[fd] == NULL || s[fd][0] == '\0'))
-		return (0);
-	return (new_line(s, line, fd, ret));
+	else
+		return ((ret == 0 && s[fd] == NULL) ? 0 : new_line(s, line, fd, ret));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,11 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: knkonzom <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/03 00:56:28 by knkonzom          #+#    #+#             */
+/*   Updated: 2019/09/03 01:40:56 by knkonzom         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 1
+# define BUFF_SIZE 100
 
-# include <stdio.h>
-# include <stdlib.h>
-# include <sys/types.h>
-# include <sys/uio.h>
 # include <fcntl.h>
 # include "libft/libft.h"
 
